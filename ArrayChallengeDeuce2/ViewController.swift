bbc27e1de29e7aebf2dfe572e6633e094e9ced73
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-08 10:45:06 -0400

    Feeling good about the line at the deli

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -12,16 +12,64 @@ class ViewController: UIViewController {
     
     // This represents our deliLine.
     var deliLine: [String] = []
+    // This is a property! Wasn't so clear to me at first since this is one of the first "official" labs coming from Objective-C
 
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
+    
     }
 
-   
+    func addNameToDeliLine(name: String) -> String {
+        var deliGreeting = ""
+        
+            if deliLine.count == 0 {
+                 deliLine.append(name)
+                deliGreeting = "Welcome \(name), you're first in line!"
+            } else if name == "Billy Crystal" {
+                deliLine.insert(name, atIndex: 0)
+                deliGreeting = "Welcome Billy! You can sit wherever you like."
+            } else if name == "Meg Ryan" {
+                deliLine.insert(name, atIndex: 0)
+                deliGreeting = "Welcome Meg! You can sit wherever you like."
+            } else {
+                deliLine.append(name)
+                deliGreeting = "Welcome \(name), you're number \(deliLine.count) in line."
+    }
+        return deliGreeting
+    }
     
+    func nowServing() -> String {
+        var serving = ""
+        
+        for name in deliLine {
+            if deliLine.count == 0 {
+                serving = "There is no one to be served."
+            } else {
+                serving = "Now serving \(name)!"
+            }
+        }
+        deliLine.removeFirst()
+        // Same as deliLine.removeAtIndex(0)
+        return serving
+    }
     
-    // Create your methods here
-
-
+    func deliLineDescription () -> String {
+        
+        var description = "The line is:"
+    
+        if deliLine.count == 0 {
+            description = "The line is currently empty."
+        }
+    
+        for (index, name) in deliLine.enumerate() {
+            if deliLine.count > 0 {
+               description += "\n\(index + 1). \(name)"
+                // Could also use description.appendContentsOf()
+            }
+        }
+        return description
+    }
 }
+// No more @end like before
 
